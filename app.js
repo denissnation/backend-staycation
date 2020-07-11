a86e7784f80638af918e0c6827a430f6f3de4434
denis Nasution <dnisnast@gmail.com>
2020-07-11 14:07:57 +0700

    cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 // mongoose connection
 const mongoose = require('mongoose');
 mongoose.connect(
@@ -40,6 +41,7 @@ app.use(
 		cookie: { maxAge: 60000 }
 	})
 );
+app.use(cors());
 app.use(flash());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
